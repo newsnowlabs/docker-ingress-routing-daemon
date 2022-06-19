7bbe72ae2062749b6d0b15e3c4f7cf3c3b7e98fd
Struan Bartlett <struan.bartlett@NewsNow.co.uk>
2022-06-19 12:05:18 +0100

    Allow launch script to launch simple DIND daemon container, and add --debug option

--- docker/create-service.sh
+++ docker/create-service.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 
+IMAGE=newsnowlabs/dind:latest
+
+if [ "$1" = "--debug" ]; then
+  shift
+  
+  DOCKER_RUN_OPTS=(--rm -it)
+else
+  DOCKER_RUN_OPTS=(-d)
+fi
+
 if [ "$1" = "manager" ]; then
   shift
   
   # Create manager container with autodetected --ingress-gateway-ips
-  CMD=(docker run --rm -it --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock newsnowlabs/dind:latest)
+  CMD=(docker run ${DOCKER_RUN_OPTS[@]} --name=dind --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock $IMAGE)
 elif [ "$1" = "manager-service" ]; then
   shift
   
   # Create manager service with autodetected --ingress-gateway-ips
-  CMD=(docker service create --name=dind --replicas=1 --constraint=node.role==manager --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock newsnowlabs/dind:latest)
+  CMD=(docker service create --name=dind --replicas=1 --constraint=node.role==manager --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock $IMAGE)
 elif [ "$1" = "global-service" ]; then
   shift
   
   # Create global service with hardcoded --ingress-gateway-ips list
-  CMD=(docker service create --name=dind-global --mode=global --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock newsnowlabs/dind:latest --global-service)
+  CMD=(docker service create --name=dind-global --mode=global --env="DOCKER_NODE_HOSTNAME={{.Node.Hostname}}" --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock $IMAGE --global-service)
+elif [ "$1" = "daemon" ]; then
+  shift
+
+  # Create DIND daemon container
+  CMD=(docker run ${DOCKER_RUN_OPTS[@]} --name=dind-child --privileged --pid=host -v /var/run/docker:/var/run/docker -v /var/run/docker.sock:/var/run/docker.sock $IMAGE --daemon)
 else
-  echo "Usage: $0 [manager|manager-service|global-service]" >&2
+  echo "Usage: $0 [--debug] [manager|manager-service|global-service|daemon]" >&2
+  echo >&2
   exit -1
 fi
 
